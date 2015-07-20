6143c3ab771da2d2239e2ea36fbc1b10a15a0312
choisungwook <kgg1959@naver.com>
2015-07-20 16:44:41 +0900

    Create PrimaryList.cpp

---
+++ list/PrimaryList.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+#define DATA int
+
+typedef struct NODE
+{
+	DATA data;
+	struct NODE* next;
+}NODE;
+
+typedef struct LinkedList
+{
+	NODE* head;
+}List;
+
+NODE* createNode(DATA data)
+{
+	NODE* newNode = (NODE*)malloc(sizeof(NODE));
+	newNode->next = NULL;
+	newNode->data = data;
+
+	return newNode;
+}
+
+void Linit(List* plist)
+{
+	plist->head = createNode(0);
+}
+
+
+int NodeCmp(int n1, int n2)
+{
+	if (n1 < n2)
+		return 1;
+	return 0;	
+}
+
+void Lpush(List *plist, DATA data)
+{
+	NODE* newNode = createNode(data);
+	NODE* cur = plist->head;
+
+	while (cur->next != NULL && NodeCmp(cur->next->data, data))
+		cur = cur->next;
+
+	newNode->next = cur->next;
+	cur->next = newNode;	
+}
+
+void Lprint(List *plist)
+{
+	NODE* cur = plist->head->next;
+
+	while (cur != NULL)
+	{
+		cout << cur->data << " ";
+		cur = cur->next;
+	}
+}
+
+int main()
+{
+	List A;
+
+	Linit(&A);
+	Lpush(&A, 4);
+	Lpush(&A, 3);
+	Lpush(&A, 1);
+	Lpush(&A, 2);
+	Lpush(&A, 9);
+
+	Lprint(&A);
+	return 0;
+}
